8886b3fd9843c490a8c96a692df8ee5d8c71a762
Your Name <you@example.com>
2017-06-29 15:06:31 -0400

    Strings practice in class

--- StringsPractice/Program.cs
+++ StringsPractice/Program.cs
@@ -38,6 +38,49 @@ namespace StringsPractice
             //{
             //    Console.WriteLine("Way to be original!");
             //}
+            //string startOfSentence = "My best friend's name is ";
+            //string endOfSentence = "Joshua.";
+
+            //Console.WriteLine(startOfSentence + endOfSentence);
+
+            //Console.WriteLine("This is a tab\t.\nThis is a backspace \\.\nThis is a bell sound.\a");
+            //Console.WriteLine("What is your first name?");
+            //string fName = Console.ReadLine();
+            //int fNameLength = fName.Length;
+
+            //Console.WriteLine("What is your last name?");
+            //string lName = Console.ReadLine();
+            //int lNameLength = lName.Length;
+
+            //if (fNameLength == lNameLength)
+            //{
+            //    Console.WriteLine("Samesis!");
+            //}
+            //else if (fNameLength > lNameLength)
+            //{
+            //    Console.WriteLine("The first name is longer.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The last name must be longer.");
+            //}
+
+            //Comparing string equality.
+            //Need to fix capitalization differences
+            Console.WriteLine("First person's name:");
+            string name1 = Console.ReadLine();
+
+            Console.WriteLine("Second person's name:");
+            string name2 = string.Format(Console.ReadLine());
+
+            if (name1 == name2)
+            {
+                Console.WriteLine("Samesis!");
+            }
+            else
+            {
+                Console.WriteLine("The names are different.");
+            }
 
         }
     }
