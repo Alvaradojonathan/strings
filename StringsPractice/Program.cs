40031699e2e67df7254046770f806b132d63b28a
Your Name <you@example.com>
2017-06-29 14:08:28 -0400

    escape

--- StringsPractice/Program.cs
+++ StringsPractice/Program.cs
@@ -10,6 +10,35 @@ namespace StringsPractice
     {
         static void Main(string[] args)
         {
+            //string firstName = "Jonathan";
+            //int lengthOfName = firstName.Length;
+            //Console.WriteLine(lengthOfName);
+
+            ////Escape characters \, 
+            //string quote = "\"I'll be back.\" -Arnold";
+            //Console.WriteLine(quote);
+
+            //string escapeString = "I want a tab after this line.\t I want a new Line.\n Hi!";
+            //string escapeString2 = "I want to show a backslash here \\ Hey what's that noise? \a";
+            //Console.WriteLine(escapeString);
+            //Console.WriteLine(escapeString2);
+
+
+            //Console.WriteLine("User 1 please type something.");
+            //string userOneInput = Console.ReadLine();
+            //Console.WriteLine("User 2 please type something.");
+            //string userTwoInput = Console.ReadLine();
+
+            ////with strings == can interchange with .Equals
+            //if (userOneInput.Equals(userTwoInput))
+            //{
+            //    Console.WriteLine("User 2 is a copy cat!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Way to be original!");
+            //}
+
         }
     }
 }
